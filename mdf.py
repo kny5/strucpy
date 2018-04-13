a1702a2f31d7073ca546f98b68155623808b79c9
kny5 <anaya.4589@gmail.com>
2018-04-13 14:45:35 -0500

    cambio por funciones y almacenamiento de variables en arreglos.

--- mdf.py
+++ mdf.py
@@ -1,33 +1,48 @@
-#entrada de datos
-#Entrada de elementos
-elem = int(input("Número de elementos: ")) #número de elementos a usar.
-mat = int(input("Número de materiales: ")) #número de materiales a usar.
+def asking(var, nombre):
+    while True:
+        try:
+            globals()[var] = int(input(str(nombre) + " <--- "))
+        except ValueError:
+            print("Ingrese un valor numérico válido.")
+            continue
+        else:
+            break
+    print("Ingresado " + str(nombre) + " = " + str(var))
+    return
+
+asking("elem","Elementos")
+asking("mat","Materiales")
+
 i = 0
-datos = [[0 for x in range(12)] for y in range(elem)] #definiendo matriz para guardar datos de elementos
+poisson = 0.25
+
+variables = [[0 for x in range(12)] for y in range(elem)] #definiendo matriz para guardar datos de elementos
+
 while (i <= elem - 1):
-    print("Datos de elemento " + str(i + 1) + ": ")
-    l_elem = int(input("   Longitud: "))
-    b_prim = int(input("   Ancho de contratrabe: "))
-    b_z = int(input("   Ancho de zapata: "))
-    h_z = int(input("   Altura de contratrabe: "))
-    poisson = 0.25
-    izz = int(input("   Inercia en eje Z: "))
-    iyy = int(input("   Inercia en eje Y: "))
-    pp_sec = int(input("   Peso propio de la sección: "))
-    nu_ang = int(input("   Angulo respecto al plano xz: "))
-    la_ang = int(input("   Angulo resecto al plano xy: "))
-    k_v = int(input("   Modulo de reacción vertical: "))
-    k_h = int(input("   Modulo de reacción horizontal: "))
-    datos[i] = [l_elem, b_prim, b_z, h_z, poisson, izz, iyy, pp_sec, nu_ang, la_ang, k_v, k_h]
+    print("\n \nDatos de elemento " + str(i + 1) + ": ")
+    asking("l_elem","Longitud")
+    asking("b_prim","Ancho de contratrabe")
+    asking("b_z","Ancho de Zapata")
+    asking("h_z","Altura de contratrabe")
+    asking("izz","Inercia eje Z")
+    asking("iyy","Inercia eje Y")
+    asking("pp_sec","Peso propio de sección")
+    asking("nu_ang","Angulo XZ")
+    asking("la_ang","Angulo XY")
+    asking("k_v","Modulo reacción Vertical")
+    asking("k_h","Modulo reacción Horizontal")
+    variables[i] = [l_elem, b_prim, b_z, h_z, poisson, izz, iyy, pp_sec, nu_ang, la_ang, k_v, k_h]
     #print(datos)
-    #i += 1 #debug point
+    #i += 1 #debug point 1
+
     i_v = 0
-    v_ens = [0 for i_v in range(12)]
+    v_ens = [[0 for x in range(i_v)] for y in range(12)]
     while(i_v <= 11):
         v_ens_name = ["dx1", "dy1", "dz1", "mx1", "my1", "mz1", "dx2", "dy2", "dz2", "mx2", "my2", "mz2"]
         v_ens[i_v] = int(input( "   " + str(v_ens_name[i_v]) + ": " ))
         i_v += 1
-    #print(v_ens)
+    #print(v_ens) #debug point 2
     i += 1
 print(datos)
+print("\n")
 print(v_ens)
