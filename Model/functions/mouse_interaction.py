68981e6578e0b8116779b10d9348895cbe401663
knyfive evifynk <anaya.4589@gmail.com>
2019-02-05 11:54:44 -0600

    OOP set for plot/render/picking program.

--- Model/functions/mouse_interaction.py
+++ Model/functions/mouse_interaction.py
@@ -32,47 +32,6 @@ def dist(x1, y1, x2, y2, x3, y3): # x3,y3 is the point
     return distance
 
 
-def _cursor(cursor):
-    maped_pos = plot_dxf.mapFromScene(cursor)
-    x_ = maped_pos.x()
-    y_ = maped_pos.y()
-    vLine.setPos(x_)
-    hLine.setPos(y_)
-
-
-def check_point(cursor):
-    global dxf, vector_set
-    _maped_pos = plot_dxf.mapFromScene(cursor.pos())
-    _x_ = _maped_pos.x()
-    _y_ = _maped_pos.y()
-    pixelsize = w1.viewPixelSize()
-
-    for vector in dxf:
-        # print("True")
-        start = vector.start_2d
-        end = vector.end_2d
-        # print("False")
-        point_toline_mindist = dist(start[0], start[1], end[0], end[1], _x_, _y_)
-        # print("maybe")
-        if point_toline_mindist < pixelsize[0] * 15:
-            vector.clicked()
-            # print("clicked ", vector.selected)
-            if vector.selected:
-                vector_set.add(vector)
-            else:
-                vector_set.remove(vector)
-            # print("setted")
-            # print('Eureka!', _x_, _y_)
-            # print("listed", vector_list)
-            if vector_set.__len__() > 0:
-                selected_vector_list = array_vectors_to_plot(vector_set)
-                plot_selected.updateData(selected_vector_list[:, 0], selected_vector_list[:, 1],
-                                         pen=pg.mkPen(color=QtGui.QColor(255, 255, 0, 200), width=5), connect="pairs")
-            else:
-                plot_selected.updateData([], [])
-            return True
-
-
 def array_vectors_to_plot(vectors):
     return np.array(reduce(add, [[vector.start_2d, vector.end_2d] for vector in vectors]))
 
@@ -81,72 +40,142 @@ def array_points_to_plot(vectors):
     return list(set([vector.start_2d for vector in vectors] + [vector.end_2d for vector in vectors]))
 
 
-# def plot_array_from_vectors(vectors):
-#    return np.array(list(map(lambda point: np.dot(Vector.last_iso_projection, point),
-# reduce(add, [[vector.start, vector.end] for vector in vectors]))))
-
-# def plot_array_from_vectors(vectors):
-#     return np.array(list(map(Vector.to_2d, reduce(add, [[vector.start, vector.end] for vector in vectors]))))
-
-
-def atm_rot(direction):
-    global dxf_mapped
-    if direction == "up":
-        Vector.alpha += 0.1
-    elif direction == "down":
-        Vector.alpha -= 0.1
-    elif direction == "left":
-        Vector.beta -= 0.1
-    elif direction == "right":
-        Vector.beta += 0.1
-
-    Vector.iso_projection()
-    dxf_mapped = array_vectors_to_plot(dxf)
-
-    plot_dxf.updateData(
-        dxf_mapped[:, 0],
-        dxf_mapped[:, 1],
-        connect="pairs")
-
-    plot_points.setData(pos=array_points_to_plot(dxf))
-
-    if vector_set.__len__() > 0:
-        selected_vectors = array_vectors_to_plot(vector_set)
-
-        plot_selected.updateData(
-            selected_vectors[:, 0],
-            selected_vectors[:, 1],
-            connect="pairs")
-
-
 class mainwin(QtGui.QMainWindow):
     keyPressed = QtCore.pyqtSignal(QtCore.QEvent)
 
     def __init__(self):
+        # global view
         super(mainwin, self).__init__()
-        # self.setGeometry(300, 300, 250, 150)
-        self.show()
+        self.setGeometry(300, 300, 800, 600)
         self.keyPressed.connect(self.on_key)
+        # self.resize(1024, 768)
+        self.vector_set = set([])
+        Vector.iso_projection()
+        self.view = pg.GraphicsLayoutWidget()  ## GraphicsView with GraphicsLayout inserted by default
+        self.view.scene().sigMouseMoved.connect(self._cursor)
+        self.view.scene().sigMouseClicked.connect(self.check_point)
+        self.setCentralWidget(self.view)
+        # self.setWindowOpacity(0.95)
+        self.setContentsMargins(-20, -20, -20, -20)
+        self.setWindowTitle('Strucpy')
+        self.show()
+        self.create_plots()
+        self.set_viewbox()
+        self.plot()
+
+    def create_plots(self):
+        self.vLine = pg.InfiniteLine(angle=90, movable=False, pen=pg.mkPen(color=QtGui.QColor(255, 33, 83, 255), width=1))
+        self.hLine = pg.InfiniteLine(angle=0, movable=False, pen=pg.mkPen(color=QtGui.QColor(255, 33, 83, 255), width=1))
+        self.plot_dxf = pg.PlotCurveItem(antialias=True, pen=pg.mkPen(color=(7, 185, 252, 255), width=2))
+        self.plot_selected = pg.PlotCurveItem(shadowPen=pg.mkPen(color=QtGui.QColor(180, 185, 252, 20), width=15),
+                                         pen=pg.mkPen(color=QtGui.QColor(100, 185, 252, 50), width=2))
+        self.plot_points = pg.ScatterPlotItem(pen=pg.mkPen(color=QtGui.QColor(57, 255, 20, 200), width=2),
+                                         brush=None, size=10, antialias=True)
+
+    def set_viewbox(self):
+        # global plot_dxf, plot_selected, plot_points, vLine, hLine
+        self.viewbox = self.view.addViewBox(invertY=False, lockAspect=1, enableMouse=True, border=pg.mkPen('k'), enableMenu=False)
+        self.viewbox.setBackgroundColor((33, 33, 33, 255))
+        self.viewbox.enableAutoRange(True)
+        self.viewbox.addItem(self.plot_dxf)
+        self.viewbox.addItem(self.plot_selected)
+        self.viewbox.addItem(self.plot_points)
+        self.viewbox.addItem(self.vLine, ignoreBounds=True)
+        self.viewbox.addItem(self.hLine, ignoreBounds=True)
+
+    def plot(self):
+        filename = 'lienzo.dxf'
+        self.dxf = read_dxf('c:/repos/strucpy/dev_files/dxf/' + filename)
+        dxf_mapped = array_vectors_to_plot(self.dxf)
+        self.plot_dxf.setData(
+            dxf_mapped[:, 0],
+            dxf_mapped[:, 1],
+            connect="pairs")
+
+        self.plot_points.setData(pos=array_points_to_plot(self.dxf))
+
+    def check_point(self, cursor):
+        _maped_pos = self.plot_dxf.mapFromScene(cursor.pos())
+        _x_ = _maped_pos.x()
+        _y_ = _maped_pos.y()
+        pixelsize = self.viewbox.viewPixelSize()
+        for vector in self.dxf:
+            # print("True")
+            start = vector.start_2d
+            end = vector.end_2d
+            # print("False")
+            point_toline_mindist = dist(start[0], start[1], end[0], end[1], _x_, _y_)
+            # print("maybe")
+            if point_toline_mindist < pixelsize[0] * 15:
+                vector.clicked()
+                # print("clicked ", vector.selected)
+                if vector.selected:
+                    self.vector_set.add(vector)
+                else:
+                    self.vector_set.remove(vector)
+                # print("setted")
+                # print('Eureka!', _x_, _y_)
+                # print("listed", vector_list)
+                if self.vector_set.__len__() > 0:
+                    selected_vector_list = array_vectors_to_plot(self.vector_set)
+                    self.plot_selected.setData(selected_vector_list[:, 0], selected_vector_list[:, 1],
+                                          pen=pg.mkPen(color=QtGui.QColor(255, 255, 0, 200), width=5), connect="pairs",
+                                          antialias=True)
+                else:
+                    self.plot_selected.updateData([], [])
+                return True
+
+    def atm_rot(self, direction):
+
+        if direction == "up":
+            Vector.alpha += 0.1
+        elif direction == "down":
+            Vector.alpha -= 0.1
+        elif direction == "left":
+            Vector.beta -= 0.1
+        elif direction == "right":
+            Vector.beta += 0.1
+
+        Vector.iso_projection()
+        dxf_mapped = array_vectors_to_plot(self.dxf)
+
+        self.plot_dxf.updateData(
+            dxf_mapped[:, 0],
+            dxf_mapped[:, 1],
+            connect="pairs")
+
+        self.plot_points.setData(pos=array_points_to_plot(self.dxf))
+
+        if self.vector_set.__len__() > 0:
+            selected_vectors = array_vectors_to_plot(self.vector_set)
+
+            self.plot_selected.updateData(
+                selected_vectors[:, 0],
+                selected_vectors[:, 1],
+                connect="pairs")
+
+    def _cursor(self, cursor):
+        maped_pos = self.plot_dxf.mapFromScene(cursor)
+        x_ = maped_pos.x()
+        y_ = maped_pos.y()
+        self.vLine.setPos(x_)
+        self.hLine.setPos(y_)
 
     def keyPressEvent(self, event):
         super(mainwin, self).keyPressEvent(event)
         self.keyPressed.emit(event)
 
-    # def mousePressEvent(self, event):
-    #     super(mainwin, self).mousePressEvent(event)
-    #     self.keyPressed.emit(event)
-
     def on_key(self, event):
         if event.key() == QtCore.Qt.Key_Up:
-            atm_rot("up")
+            self.atm_rot("up")
         elif event.key() == QtCore.Qt.Key_Down:
-            atm_rot("down")
+            self.atm_rot("down")
         elif event.key() == QtCore.Qt.Key_Left:
-            atm_rot("left")
+            self.atm_rot("left")
         elif event.key() == QtCore.Qt.Key_Right:
-            atm_rot("right")
+            self.atm_rot("right")
         if event.key() == QtCore.Qt.Key_Enter:
-            atm_rot("up")
+            self.atm_rot("up")
             print("hello")# this is called whenever the continue button is presse
         elif event.key() == QtCore.Qt.Key_Q:
             print("Killing")
@@ -156,71 +185,22 @@ class mainwin(QtGui.QMainWindow):
     # def proceed(direction):
     #     # print("Moviendo a " + str(direction))
     #     atm_rot(direction)
+# app = QtGui.QApplication([])
+# app.setOverrideCursor(QtCore.Qt.CrossCursor)
+# mw = mainwin()
+# pg.setConfigOptions(antialias=True, useOpenGL=True)
+# Vector.iso_projection()
 
 
-app = QtGui.QApplication([])
-# app.setOverrideCursor(QtCore.Qt.BlankCursor)
-app.setOverrideCursor(QtCore.Qt.CrossCursor)
-mw = mainwin()
-mw.resize(1024, 768)
-view = pg.GraphicsLayoutWidget()  ## GraphicsView with GraphicsLayout inserted by default
-mw.setCentralWidget(view)
-# mw.setWindowOpacity(0.95)
-mw.setContentsMargins(-20,-20,-20,-20)
-mw.show()
-mw.setWindowTitle('Strucpy')
-pg.setConfigOptions(antialias=True, useOpenGL=True)
-w1 = view.addViewBox(invertY=False, lockAspect=1, enableMouse=True, border=pg.mkPen('k'), enableMenu=False)
-w1.setBackgroundColor((33, 33, 33, 255))
-w1.enableAutoRange(True)
-filename = 'lienzo.dxf'
-dxf = read_dxf('c:/repos/strucpy/dev_files/dxf/' + filename)
-
-Vector.iso_projection()
-dxf_mapped = array_vectors_to_plot(dxf)
-
-plot_dxf = pg.PlotCurveItem()
-plot_dxf.setData(
-    dxf_mapped[:, 0],
-    dxf_mapped[:, 1],
-    connect="pairs",
-    antialias=True,
-    # setCompositionMode=3,
-    pen=pg.mkPen(color=(7, 185, 252, 255), width=2))
-#   brush=pg.mkBrush(7, 185, 252, 100)
-#   )
-w1.addItem(plot_dxf)
-
-vector_set = set([])
-
-plot_selected = pg.PlotCurveItem(
-    shadowPen=pg.mkPen(color=QtGui.QColor(180, 185, 252, 20), width=15, setCompositionMode=QtGui.QPainter.CompositionMode(2)),
-    pen=pg.mkPen(color=QtGui.QColor(100, 185, 252, 50), width=2, style=QtCore.Qt.DashLine))
-w1.addItem(plot_selected)
-
-plot_points = pg.ScatterPlotItem(
-    pos=array_points_to_plot(dxf),
-    pen=pg.mkPen(
-        color=QtGui.QColor(57, 255, 20, 200),
-        width=2),
-    brush=None,
-    size=10,
-    antialias=True)
-w1.addItem(plot_points)
-
-# location = view.addLabel(filename, 1, 0.1)  # check position arguments or any other argument to avoid overlapping
-vLine = pg.InfiniteLine(angle=90, movable=False, pen=pg.mkPen(color=QtGui.QColor(255, 33, 83, 255), width=1))
-hLine = pg.InfiniteLine(angle=0, movable=False, pen=pg.mkPen(color=QtGui.QColor(255, 33, 83, 255), width=1))
-w1.addItem(vLine, ignoreBounds=True)
-w1.addItem(hLine, ignoreBounds=True)
-
-view.scene().sigMouseMoved.connect(_cursor)
-view.scene().sigMouseClicked.connect(check_point)
-# mw.sigMouseClicked.connect(lambda x: print("hello"))
+class app(QtGui.QApplication):
+    def __init__(self):
+        super().__init__([])
+        self.setOverrideCursor(QtCore.Qt.CrossCursor)
+        self.mainwindow = mainwin()
 
 
 if not __name__ == '__main__':
     import sys
-
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
+        app = app()
+        app.exec_()
